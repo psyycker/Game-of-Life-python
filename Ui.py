6e640b72d7a9fd87733b522cabbac45f518d6bbc
remy <remy.villulles@gmail.com>
2019-02-12 15:24:35 +0000

    Things

---
+++ Ui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+
+class Ui:
+
+    def __init__(self):
+        print("the ui")
+        self.__masterWin = Tk("Game of Life")
+
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from Core import Core
+from Ui import Ui
+
 
 def main():
-    print("Starting Game of Life")
-    coreInst = Core(40, 40, 100)
-    coreInst.setDebug(False)
-
-    while coreInst.iterate() is True:
-        pass
-    print(coreInst.getLivingCellsCount())
-    print(coreInst.getDeadCellsCount())
+    # print("Starting Game of Life")
+    # coreInst = Core(40, 40, 100)
+    # coreInst.setDebug(False)
+    #
+    # while coreInst.iterate() is True:
+    #     pass
+    # print(coreInst.getLivingCellsCount())
+    # print(coreInst.getDeadCellsCount())
+    ui = Ui()
 
 
 if __name__ == '__main__':
